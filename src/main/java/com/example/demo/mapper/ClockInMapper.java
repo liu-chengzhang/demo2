9e45f4bbb55bd45a4a50ba820e6a823d8334b273
2021302111106 <2021302111106@whu.edu.cn>
2023-06-27 15:02:57 +0800

    -

--- src/main/java/com/example/demo/mapper/ClockInMapper.java
+++ src/main/java/com/example/demo/mapper/ClockInMapper.java
@@ -2,11 +2,12 @@ package com.example.demo.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
 
 @Mapper
 public interface ClockInMapper {
     @Insert("Insert into clockintb values(#{ClockInId},#{ClockInTime},#{ClockInDate},#{Content})")
-    public boolean clockin(Integer ClockInId, Integer ClockInTime, Date ClockInDate,String Content);
+    public boolean clockin(@Param("ClockInId") Integer ClockInId, @Param("ClockInTime") Integer ClockInTime,@Param("ClockInDate") Date ClockInDate,@Param("Content") String Content);
 }
